8382f8ba104b082fc1734de888459834243bcb5c
刘华秋 <liu13269455505@126.com>
2018-04-02 21:23:16 +0800

    项目迁移

---
+++ game/Assets/Scripts/Forward.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forward : MonoBehaviour
+{
+    private float timetodestroy;
+
+    // Use this for initialization使用它来初始化
+    void Start()
+    {
+        //GameObject.rigidbody.constantForce游戏对象、刚体、恒力
+        timetodestroy = 2;
+    }
+
+    // Update is called once per frame逐帧召唤更新一次
+    void Update()
+    {
+        timetodestroy = timetodestroy - Time.deltaTime;
+        //gameObject.transform.position += transform.forward * Time.deltaTime * 30;
+        if (timetodestroy < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        string name = col.gameObject.name;
+        if (name.Contains("Soldier") || name.Contains("Mesh") || name.Contains("Barre") )
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ game/Assets/Scripts/Soldier.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour {
+
+    Dictionary<string, int> bag =  new Dictionary<string, int>();
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Dictionary<string, int> get_bag()
+    {
+        return bag;
+    }
+
+    public void add_prop(string prop_name, int prop_num)
+    {
+        if (bag.ContainsKey(prop_name))
+        {
+            bag[prop_name] += prop_num;
+        }
+        else
+        {
+            bag.Add(prop_name, prop_num);
+        }
+    }
+
+    public bool use_prop(string prop_name, int prop_num)
+    {
+        if (bag.ContainsKey(prop_name))
+        {
+            if(bag[prop_name] >= prop_num)
+            {
+                bag[prop_name] -= prop_num;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
