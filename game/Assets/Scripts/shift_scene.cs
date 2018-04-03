21744f7e2f966b1b4d7adc2cec5cbd8a392d68ed
zzhywzq <2949052194@qq.com>
2018-04-03 14:50:38 +0800

    更新
    Asset里新建文件夹Scenes用于存放场景，完成场景切换，退出游戏

---
+++ game/Assets/Scripts/shift_scene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;//增加命名空间
+
+public class shift_scene : MonoBehaviour
+{
+    public void OnStartGame(string sceneName)
+    {
+        SceneManager.LoadScene(1);//1是场景的索引
+    }
+}
