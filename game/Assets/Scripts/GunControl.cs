97ef4286821edc0b73985399037f09e02bfd7464
zmt <444638290@qq.com>
2018-04-04 08:37:31 +0800

    换枪脚本

---
+++ game/Assets/Scripts/GunControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour {
+    GunComponent[] gunControllers;
+    int index = 0;
+    // Use this for initialization
+    void Start () {
+        gunControllers = transform.GetComponentsInChildren<GunComponent>();
+        for (int i = 1; i < gunControllers.Length; i++)
+        {
+            gunControllers[i].gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        //换枪
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+          changeGun(gunControllers[2].gameObject);
+        }
+
+    }
+    GameObject changeGun(GameObject g)
+    {
+        int before = index;
+        int i = 0;
+        while (gunControllers[i].gameObject.name!=g.name && i < gunControllers.Length)
+        {
+            i++;
+        }
+        index = i;
+        gunControllers[before].gameObject.SetActive(false);
+        gunControllers[index].gameObject.SetActive(true);
+        return gunControllers[before].gameObject;
+    }
+
+}
