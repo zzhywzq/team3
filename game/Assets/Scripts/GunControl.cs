65be30be2dd6647392b0732be48f648f8d1046c6
zzhywzq <2949052194@qq.com>
2018-04-08 18:22:58 +0800

    更新

---
+++ game/Assets/Scripts/GunControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour {
+    GunComponent[] gunControllers;
+    int index = 0;
+    // Use this for initialization
+    void Start () {
+        gunControllers = transform.GetComponentsInChildren<GunComponent>();
+        for (int i = 1; i < gunControllers.Length; i++)
+        {
+            gunControllers[i].gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        //换枪
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(gunControllers[0].gameObject.activeSelf==true)
+                changeGun(gunControllers[1].gameObject);
+            else if (gunControllers[1].gameObject.activeSelf == true)
+                changeGun(gunControllers[0].gameObject);
+        }
+
+    }
+    GameObject changeGun(GameObject g)
+    {
+        int before = index;
+        int i = 0;
+        while (gunControllers[i].gameObject.name!=g.name && i < gunControllers.Length)
+        {
+            i++;
+        }
+        index = i;
+        gunControllers[before].gameObject.SetActive(false);
+        gunControllers[index].gameObject.SetActive(true);
+        return gunControllers[before].gameObject;
+    }
+
+}
---
+++ game/Assets/Scripts/Soldier.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour {
+
+    Dictionary<string, int> bag =  new Dictionary<string, int>();
+    //键值对数组 <名字，数量>
+
+    public int defend = 5;
+    public int jump_high = 5;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Dictionary<string, int> get_bag()
+    {
+        return bag;
+    }
+
+    public void add_prop(string prop_name, int prop_num)
+    {
+        if (bag.ContainsKey(prop_name))
+        {
+            bag[prop_name] += prop_num;//物品数加一
+        }
+        else
+        {
+            bag.Add(prop_name, prop_num);
+        }
+    }
+
+    //public bool use_prop(string prop_name, int prop_num)
+    //{
+    //    if (bag.ContainsKey(prop_name))
+    //    {
+    //        if (bag[prop_name] >= prop_num)
+    //        {
+    //            bag[prop_name] -= prop_num;
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
+
+}
