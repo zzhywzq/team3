2f00dd189fe72e5f2f0359818e611537f6d11ded
Eva-15301032 <2305937542@qq.com>
2018-04-11 21:10:24 +0800

    减小空间复杂度，降低耦合度，实现按键弹出隐藏背包

---
+++ game/Assets/Scripts/Show_Or_Hide.cs
@@ -0,0 +1,32 @@
+﻿  
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Show_Or_Hide : MonoBehaviour
+{
+    GameObject obj;
+    void Start()
+    {
+        obj = GameObject.Find("Image1");
+        obj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            show_hide();
+        }
+    }
+    public void show_hide()//显示或隐藏
+    {
+        if (obj.activeSelf == true)
+            obj.SetActive(false);
+        else if (obj.activeSelf == false)
+            obj.SetActive(true);
+    }
+}
+
